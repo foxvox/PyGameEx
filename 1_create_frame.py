5f8381c03804b49e4e50a5fc52e97d6932974f65
alex <windev@nate.com>
2024-03-13 10:12:39 +0900

    변경

--- 1_create_frame.py
+++ 1_create_frame.py
@@ -12,6 +12,5 @@ running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False; 
-         
+            running = False
 pygame.quit() 
